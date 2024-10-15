9ff7bd22fdc6c9a66f77f1a6e523a36e1dacea5c
jbanimineni <jbanimineni@brigadeworks.com>
2024-10-15 15:13:28 +0530

    initial commit

---
+++ kops/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from kops.users.models import User
+from kops.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
